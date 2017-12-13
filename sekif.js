a2559ad926ff42899b842e3c3527cdcb4fbde39a
Asyeif <32222060+Asyeif@users.noreply.github.com>
2017-12-13 16:00:40 +0700

    Update sekif.js

--- sekif.js
+++ sekif.js
@@ -20,7 +20,11 @@ j++;}
 if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
 var x=Math.floor((Math.random()*4)+ 1);
-a_to_ve[i].href="https://www.sekif.blogspot.com/p/go.html?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+var xxx=null;
+if(x=="1"){xxx="1.html"}
+if(x=="2"){xxx="2.html"}
+if(x=="3"){xxx="3.html"}
+if(x=="4"){xxx="4.html"}a_to_ve[i].href="http://www.kompisafelinkv2.ga/p/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
